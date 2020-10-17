1144ba81c989a219103a380d4f12d56efbd2286f
Luigi Moraes <luigimoraes3@gmail.com>
2020-10-16 22:29:09 -0300

    fixed bugs

--- controller/partidoController.js
+++ controller/partidoController.js
@@ -3,10 +3,8 @@ const parser = require('body-parser');
 const fs = require('fs');
 
 exports.selectPartidos = function(req, res){
-	db.selectAllPartidos(() => {
-		let jsonFile = fs.readFileSync('./model/partidos.json', 'utf-8');
-		let conv = JSON.parse(jsonFile);
-		res.send(conv);
+	db.selectAllPartidos((jsonString) => {
+		res.send(jsonString);
 	});
 };
 
